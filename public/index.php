198304595cde13e8bc20a3bbb4d2f5998d53f785
Emilia Nuutila <emilia.nuutila@edu.sasky.fi>
2023-03-06 18:12:38 +0200

    hakemistoviittaukset vakioiksi

--- public/index.php
+++ public/index.php
@@ -8,7 +8,8 @@ require_once '../src/init.php';
   $request = str_replace($config['urls']['baseUrl'],'',$_SERVER['REQUEST_URI']);
   $request = strtok($request, '?');
   // Luodaan uusi Plates-olio ja kytketään se sovelluksen sivupohjiin.
-  $templates = new League\Plates\Engine('../src/view');
+    $templates = new League\Plates\Engine(TEMPLATE_DIR);
+
 
   // Selvitetään mitä sivua on kutsuttu ja suoritetaan sivua vastaava
   // käsittelijä.
