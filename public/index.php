0f2c2c2a7c397bb1217cb5bd09b7e7bad5d508ae
Emilia Nuutila <emilia.nuutila@edu.sasky.fi>
2023-03-06 18:37:59 +0200

    tapahtumat haetaan tietokannasta sivupohjaan

--- public/index.php
+++ public/index.php
@@ -14,8 +14,10 @@ require_once '../src/init.php';
   // Selvitetään mitä sivua on kutsuttu ja suoritetaan sivua vastaava
   // käsittelijä.
   if ($request === '/' || $request === '/tapahtumat') {
-    echo $templates->render('tapahtumat');
-  } else if ($request === '/tapahtuma') {
+    require_once MODEL_DIR . 'tapahtuma.php';
+    $tapahtumat = haeTapahtumat();
+    echo $templates->render('tapahtumat',['tapahtumat' => $tapahtumat]);
+}   else if ($request === '/tapahtuma') {
     echo $templates->render('tapahtuma');
   } else {
     echo $templates->render('notfound');
