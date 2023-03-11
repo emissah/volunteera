9f91bf8125fa08bb597b8e4116941658109ae12f
Emilia Nuutila <emilia.nuutila@edu.sasky.fi>
2023-03-12 01:11:13 +0200

    templates-tiedoston muokkaus, kuvien lisääminen ja linkkien lisäys, tyylittely

--- src/view/template.php
+++ src/view/template.php
@@ -14,17 +14,18 @@
             echo "<div>$_SESSION[user]</div>";
             echo "<div><a href='logout'>Kirjaudu ulos</a></div>";
           } else {
-            echo "<div><a href='kirjaudu'>Kirjaudu</a></div>";
+            echo "<div><a href='kirjaudu'>Kirjaudu sisään</a></div>";
+            echo "<div><a href='lisaa_tili'>Rekisteröidy</a></div>";
           }
         ?>
       </div>
     </header>
 
    <nav>
-   <a href="">Tapahtumat</a>
-   <a href="">Tietoa sivusta</a>
-   <a href="">Yhteystiedot</a>
-   <a href="">Palautelomake</a>
+   <a href="tapahtumat">Tapahtumat</a>
+   <a href="tietoa">Tietoa sivusta</a>
+   <a href="yhteystiedot">Yhteystiedot</a>
+   <a href="palaute">Palautelomake</a>
   </nav>
 
     <section>
@@ -32,7 +33,13 @@
     </section>
     <footer>
       <hr>
-      <div>Volunteera by Emikki</div>
+        <div class="icons">
+      <a href="https://www.facebook.com"><img  src="images/logo-facebook.svg" height="75" width="75"></a>
+      <a href="https://www.instagram.com"><img src="images/logo-instagram.svg" height="75" width="75"></a>
+      <a href="https://www.twitter.com"><img src="images/logo-twitter.svg" height="75" width="75"></a>
+        </div>
+    <p class="seuraa">Seuraa meitä sosiaalisessa mediassa!</p>
+    <div class="footer-text">Volunteera by Emikki</div>
     </footer>
   </body>
 </html>
