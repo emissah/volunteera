e639c0f8cb0eb6bf6b1afd0b0b93cb27738bd228
Emilia Nuutila <emilia.nuutila@edu.sasky.fi>
2023-03-11 21:00:27 +0200

    vaihtoavaimen lisääminen henkilo-tauluun

--- src/model/henkilo.php
+++ src/model/henkilo.php
@@ -23,4 +23,17 @@
     return DB::run('UPDATE henkilo1 SET vahvistettu = TRUE WHERE vahvavain = ?', [$avain])->rowCount();
   }
 
+  function asetaVaihtoavain($email,$avain) {
+    return DB::run('UPDATE henkilo1 SET nollausavain = ?, nollausaika = NOW() + INTERVAL 30 MINUTE WHERE email = ?', [$avain,$email])->rowCount();
+  }
+
+  function tarkistaVaihtoavain($avain) {
+    return DB::run('SELECT nollausavain, nollausaika-NOW() AS aikaikkuna FROM henkilo1 WHERE nollausavain = ?', [$avain])->fetch();
+  }
+
+  function vaihdaSalasanaAvaimella($salasana,$avain) {
+    return DB::run('UPDATE henkilo1 SET salasana = ?, nollausavain = NULL, nollausaika = NULL WHERE nollausavain = ?', [$salasana,$avain])->rowCount();
+  }
+
+
 ?>
